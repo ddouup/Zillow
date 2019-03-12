28be962d5434f408dd76e874d07645da92ed4585
DDOU <ddou@connect.ust.hk>
2019-03-12 12:22:14 +0800

    update

--- merge.py
+++ merge.py
@@ -8,20 +8,30 @@ def main():
     dataframe = pd.DataFrame()
     count = 0
 
+    info = pd.DataFrame(columns=['State', 'Number', 'Percentage'])
+
     for datafile in sorted(glob.glob("./processed_data/*.csv")):
         print("Processing ", datafile)
 
         df = pd.read_csv(datafile)
         dataframe = dataframe.append(df, ignore_index=True)
 
+        state = os.path.basename(datafile).split('.')[0]
+        info = info.append({'State': state, 'Number': df.shape[0], 'Percentage': None}, ignore_index=True)
         count += df.shape[0]
 
     dataframe.reset_index()
     print(dataframe)
-
+    
+    for i, row in info.iterrows():
+        percentage = info.at[i,'Number']/float(count)
+        info.loc[[i], ['Percentage']] = percentage
+    
+    print(info)
     path = './house_data.csv'
     print("Save to: ", path)
-    dataframe.to_csv(path, index=False)
+    #dataframe.to_csv(path, index=False)
+    #info.to_csv('./info.csv', index=False)
 
     print("Total Number of Houses: ", count)
 
